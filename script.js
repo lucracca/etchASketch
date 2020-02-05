33cdaeee02910a28e6d19ad390a03e67beb2e572
Luc Racca <lucracca@gmail.com>
2020-02-05 10:51:12 +0800

    added responsive grid. Changed display to inline block.

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ const container = document.getElementById("container");
 
 function generate() {
     container.innerHTML = ""; //a quick way to clear the past generated divs
-    const cols = parseInt(document.getElementById("colsTa").value);
-    const rows = parseInt(document.getElementById("rowsTa").value);
+    const size = parseInt(document.getElementById("sizeTa").value);
 
-    for (i = 0; i < cols; i++) {
-        for (j = 0; j < rows; j++) {
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
             const div = document.createElement("div");
-            div.style.width = "25px";
-            div.style.height = "25px";
+            div.style.width = (900 / size) * .98 + "px";
+            div.style.height = (900 / size) * .98 + "px";
             div.style.background = "blue";
             div.addEventListener("mouseover", (e) => {
                 div.style.background = "red";
